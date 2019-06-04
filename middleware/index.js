20d3891170e94df352f90fdbb2f37dfba72b33c5
Chakradhar <chakradharvasurama@gmail.com>
2019-06-04 20:22:32 +0530

    forgot password

--- middleware/index.js
+++ middleware/index.js
@@ -12,7 +12,7 @@ if(req.isAuthenticated()){
         } 
         else{
             //does the user own campground
-            if(foundCampground.author.id.equals(req.user._id)){
+            if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin){
                 next();
             }
             else{
@@ -37,7 +37,7 @@ middleWareObj.checkCommentOwnership = function(req,res,next){
             } 
             else{
                 //does the user own campground
-                if(foundComment.author.id.equals(req.user._id)){
+                if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin){
                     next();
                 }
                 else{
--- models/user.js
+++ models/user.js
@@ -3,7 +3,14 @@ const mongoose=require("mongoose"),
 
 const UserSchema = new mongoose.Schema({
         username: String,
-        password: String
+        password: String,
+        avatar:String,
+        firstName:String,
+        lastName:String,
+        email:{type: String, unique: true, required: true},
+        resetPasswordToken: String,
+    resetPasswordExpires: Date,
+        isAdmin:{type:Boolean, default:false}
     });
 UserSchema.plugin(passportLocalMongoose);
 module.exports=mongoose.model("User",UserSchema);
